933755bbc6e14f5c40ef60abe23432b674a32007
9S <git@springer.dev>
2022-09-05 23:37:16 +0200

    persistence: use jakarta annotation instead of javax
    Java EE now belongs to the Eclipse Foundation and was renamed to Jakarta

--- isy-persistence/src/main/java/de/bund/bva/isyfact/persistence/config/OracleDataSourceProperties.java
+++ isy-persistence/src/main/java/de/bund/bva/isyfact/persistence/config/OracleDataSourceProperties.java
@@ -1,8 +1,7 @@
 package de.bund.bva.isyfact.persistence.config;
 
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.validation.annotation.Validated;
 
 /**
